febb07f276523eebf43a8d2db11de9f9d7b09494
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-11-14 17:27:39 +0100

    Header and sequence added

--- karvonen.el
+++ karvonen.el
@@ -2,11 +2,27 @@
 ;;; Karvonen Heart Rate
 
 (defconst karvonen--buffer-name "* Karvonen Output *")
+(defconst karvonen--left-column-size 15)
 
-(defun karvonen-formula (age restingHR intensity)
+(defun karvonen-formula (restingHR age intensity)
   (+ (* (- 220 age restingHR)
         intensity)
      restingHR))
+
+(defun karvonen-create--loop (min max)
+  (let ((value min)
+        (output (list))) 
+    (while (< value max)
+      (setq output (append (list value) output))
+      (setq value (+ value 5)))
+    (reverse output)))
+
+(defun karvonen--header (restingHR age)
+  (concat (format "Resting Pulse: %d  Age: %d\n" restingHR age)
+          "\n"
+          (format (concat "%-" (number-to-string karvonen--left-column-size) "s|%s") "Intensity" "Rate\n")
+          (make-string karvonen--left-column-size ?-) "+------\n"))
+
 
 
 (defun karvonen--show-buffer ()
